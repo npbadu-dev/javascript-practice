1e6b1313161b78bdabd8492e2e84d021a6d308fe
npbadu-dev <niteshprasadbadu@gmail.com>
2026-06-27 15:50:19 +0545

    data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,14 +1,35 @@
-
 // Linking a JavaScript File
+/*
 let js = "amazing";
-// if (js === 'amazing'){
-//     alert("JavaScript is Fun!");
-// }
+if (js === "amazing") {
+  alert("JavaScript is Fun!");
+}
 console.log(40 + 8 + 23 - 10);
+*/
 
 // 05. Values and Variables
+/*
 let firstName = "Nitesh";
 console.log(firstName);
 
 let myFirstJob = "student";
 let myCurrentJob = "learner";
+*/
+
+// 06. Data Types
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof(javascriptIsFun));
+
+javascriptIsFun = "YES!";
+// console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;  //not assigning value to year = undefined
+console.log(year);
+console.log(typeof year)
+
+year = 2007; //assigning the value to year = now type change
+console.log(typeof year)
