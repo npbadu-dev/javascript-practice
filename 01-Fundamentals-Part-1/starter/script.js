242e30890d9ff21504487a2b323602087691e140
npbadu-dev <niteshprasadbadu@gmail.com>
2026-06-27 13:16:06 +0545

    Values & Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,14 @@
-////////////////////////////////////
+
 // Linking a JavaScript File
 let js = "amazing";
-if (js === 'amazing'){
-    alert("JavaScript is Fun!");
-}
-
+// if (js === 'amazing'){
+//     alert("JavaScript is Fun!");
+// }
 console.log(40 + 8 + 23 - 10);
+
+// 05. Values and Variables
+let firstName = "Nitesh";
+console.log(firstName);
+
+let myFirstJob = "student";
+let myCurrentJob = "learner";
