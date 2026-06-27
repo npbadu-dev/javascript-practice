9a20a64e93c9504b3858cda245221aa0514eae2e
npbadu-dev <niteshprasadbadu@gmail.com>
2026-06-27 11:38:56 +0545

    linking js file

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,8 @@
+////////////////////////////////////
+// Linking a JavaScript File
+let js = "amazing";
+if (js === 'amazing'){
+    alert("JavaScript is Fun!");
+}
+
+console.log(40 + 8 + 23 - 10);
