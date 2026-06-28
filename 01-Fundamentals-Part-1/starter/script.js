b2ca6ee16e3f793bab722daff29f7a9e6723ea1e
npbadu-dev <niteshprasadbadu@gmail.com>
2026-06-28 16:38:02 +0545

    Ternary operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -379,3 +379,23 @@ if (day === "monday") {
 }
 */
 
+// Conditional (Ternary) Operators
+
+const age = 18;
+age >= 18
+  ? console.log("I like to drink wine 🍷")
+  : console.log("I like to drink water 🥤");
+
+// more useful case of ternary operators
+const drink = age >= 18 ? "wine 🍷" : "water 🥤";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine 🍷";
+} else {
+  drink2 = "water 🥤";
+}
+console.log(drink2);
+console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 🥤"}`);
+console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 🥤"}`);
