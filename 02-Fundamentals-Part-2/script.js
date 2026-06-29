b1a67a1494090079cf7f372b880bc1b786e257fb
npbadu-dev <niteshprasadbadu@gmail.com>
2026-06-29 07:46:33 +0545

    Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,16 @@
+// Activating Strict Mode
+
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriverLicense = true;
+}
+if(hasDriversLicense){
+    console.log("You Can Drive :D");
+}
+// strict mode reserved this word: interface, private..
+// const interface = "Audio";
+// const private = 534;
