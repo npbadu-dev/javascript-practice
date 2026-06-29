2a4666f4eeb4e94ec1054f946ace73fda892a7d3
npbadu-dev <niteshprasadbadu@gmail.com>
2026-06-29 08:02:30 +0545

    Function

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,5 +1,5 @@
 // Activating Strict Mode
-
+/*
 "use strict";
 
 let hasDriversLicense = false;
@@ -14,3 +14,29 @@ if(hasDriversLicense){
 // strict mode reserved this word: interface, private..
 // const interface = "Audio";
 // const private = 534;
+*/
+
+// Function
+/*
+function logger() {
+  console.log("My name is Nitesh!");
+}
+
+// calling or running or invoking the function.
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+
