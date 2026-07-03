48ee5ea814497dbb4cf52ba629db260dac188a32
npbadu-dev <niteshprasadbadu@gmail.com>
2026-07-03 09:36:06 +0545

    Intro to for Loop

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -446,5 +446,11 @@ if (john.bmi > mark.bmi) {
 }
 */
 
-
-
+// ******************************************************
+// Iteration The for Loop
+// ******************************************************
+/*
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+*/
