b2a19ecc252fd0bb3c3d0de9c524b86a6e29eaf3
npbadu-dev <niteshprasadbadu@gmail.com>
2026-07-03 16:58:24 +0545

    Backward Loop & Loop in Loop

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -505,4 +505,28 @@ for (let i = 0; i < niteshArray.length; i++) {
 }
 */
 
+// **************************************************
+// Looping Backwards & Loop in Loop
+// **************************************************
+/*
+const nitesh = [
+  "nitesh",
+  "badu",
+  2037 - 2007,
+  "student",
+  ["Parbat", "Sushant", "Anita"],
+];
+
+for (let i = nitesh.length - 1; i >= 0; i--) {
+  console.log(i, nitesh[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`--------Starting exercise ${exercise}--------`);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+  }
+}
+*/
+
 
