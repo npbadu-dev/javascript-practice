ae16f470ea699267a997ecb329b915ecca5f687a
npbadu-dev <niteshprasadbadu@gmail.com>
2026-07-03 17:19:59 +0545

    While Loop

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -529,4 +529,25 @@ for (let exercise = 1; exercise < 4; exercise++) {
 }
 */
 
+// ********************************************************
+// While Loop
+// ********************************************************
+/*
+// let rep = 1;
+// while (rep <= 10) {
+//   console.log(`Lifting weights repetition ${rep}`);
+//   rep++;
+// }
 
+let dice = Math.trunc(Math.random() * 6) + 1;
+// console.log(dice);
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}.`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+
+  if (dice === 6) {
+    console.log("Loop is about to end...");
+  }
+}
+*/
