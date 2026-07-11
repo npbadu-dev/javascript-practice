cbcb5ab19a1aa612f0f49baec457b61bd5adf1b5
npbadu-dev <niteshprasadbadu@gmail.com>
2026-07-11 17:10:10 +0545

    CHALLENGE #1

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -117,4 +117,46 @@ const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
 console.log(amplitudeBug);
 */
 
-// *********
+// ****************************************************
+// CHALLENGE #1
+// ****************************************************
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+// 1) Understanding the Problem
+// - Array transformed to string, and separated by ...
+// - What is the X days? Answer: index + 1
+
+// 2) Breaking up into sub-problems
+// - Transform array into string
+// - Transform each element to string with ºC
+// - Strings need to contain day (index + 1)
+// - Add ... b/w elements and start and end of string
+// - Log string to console
+/*
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = "... ";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log(str);
+  // console.log("..." + str);
+};
+printForecast(data1);
+printForecast(data2);
+*/
+
+
