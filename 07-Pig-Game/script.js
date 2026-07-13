ab75c25a11e884e5c16de50f7a80332ede437a38
npbadu-dev <niteshprasadbadu@gmail.com>
2026-07-13 14:13:34 +0545

    switching player

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -3,6 +3,8 @@
 // Selecting Elements
 
 // const score0El = document.querySelector("#score--0");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.getElementById("score--0");
 const score1El = document.querySelector("#score--1");
 const current0El = document.getElementById("current--0");
@@ -18,7 +20,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling Dice Functionality
 btnRoll.addEventListener("click", function () {
@@ -33,9 +37,15 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   }
   //   Switch to next player
   else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
