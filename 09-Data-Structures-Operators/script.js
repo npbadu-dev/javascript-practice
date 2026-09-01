0e8f85168e89253c727490dfa9d4f37dd9b4b2da
npbadu-dev <niteshprasadbadu@gmail.com>
2026-09-01 17:30:45 +0545

    20: maps iteration

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -80,6 +80,47 @@ const restaurant = {
   },
 };
 
+
+
+// ************* Maps Iteration ****************
+/*
+const question = new Map([
+  ["question", "What is the best programming language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct :)"],
+  [false, "Try Again!"],
+]);
+console.log(question);
+
+// Convert Objects to Map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz App
+console.log(question.get("question"));
+for (const [key, value] of question) {
+  if (typeof key === "number") {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+// const answer = Number(prompt("Your answer"));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get("correct") === answer));
+
+// Convert Map into Array
+console.log([...question]);
+// console.log(question.entries());
+
+console.log([...question.keys()]);
+console.log([...question.values()]);
+*/
+
 // ******************* Maps *****************
 /*
 const rest = new Map();
