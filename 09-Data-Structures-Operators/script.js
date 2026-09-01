d5e4ce58b5821669c47ee115a844778e0f84e976
npbadu-dev <niteshprasadbadu@gmail.com>
2026-09-01 23:23:09 +0545

    nothing

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -81,6 +81,7 @@ const restaurant = {
 };
 
 // *************** String Method Practice ******************
+/*
 const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
@@ -99,6 +100,7 @@ for (const flight of flights.split("+")) {
 
   console.log(output);
 }
+*/
 
 // != *************** Challenge 4 ***************************
 /*
