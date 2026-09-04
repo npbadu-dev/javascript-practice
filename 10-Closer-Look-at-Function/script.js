f42f29922dbae6b26f04cbe7439e94d923c887a2
npbadu-dev <niteshprasadbadu@gmail.com>
2026-09-04 09:52:21 +0545

    Default parameters

--- 10-Closer-Look-at-Function/script.js
+++ 10-Closer-Look-at-Function/script.js
@@ -1,1 +1,32 @@
-'use strict';
+"use strict";
+
+//  ***************** Default Parameters *******************
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers,
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking("LH123");
+createBooking("LH123", 2, 800);
+createBooking("LH123", 5);
+createBooking("LH123", 10);
+
+// createBooking("LH123", 1000); //directly skipping no. of passengers, writing price
+createBooking("LH123", undefined, 1000);
+*/
+
