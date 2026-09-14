dc7b212d0532f363e2f7fc0e31d45cff245cdbc6
npbadu-dev <niteshprasadbadu@gmail.com>
2026-09-14 11:50:43 +0545

    more example of closure

--- 10-Closer-Look-at-Function/script.js
+++ 10-Closer-Look-at-Function/script.js
@@ -302,7 +302,7 @@ console.log(notPrivate);
 */
 
 // ******************** Closure ***********************
-
+/*
 const secureBooking = function () {
   let passengerCount = 0;
 
@@ -317,3 +317,44 @@ booker();
 booker();
 
 console.dir(booker);
+*/
+
+// ************* More Example of Closure *****************
+/*
+// Example 1
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
+console.dir(f);
+
+// Re-assign
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardingPassenger = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers.`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds.`);
+};
+
+const perGroup = 1000;
+boardingPassenger(180, 3);
+*/
