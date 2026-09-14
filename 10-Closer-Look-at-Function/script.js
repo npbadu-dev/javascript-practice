26db3eab2c021f56fa5d67a40e02491377b50630
npbadu-dev <niteshprasadbadu@gmail.com>
2026-09-14 11:58:13 +0545

    Challenge 2

--- 10-Closer-Look-at-Function/script.js
+++ 10-Closer-Look-at-Function/script.js
@@ -358,3 +358,25 @@ const boardingPassenger = function (n, wait) {
 const perGroup = 1000;
 boardingPassenger(180, 3);
 */
+
+// !----------- CHALLENGE 2 ----------------
+/*
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+
+  const body = document.querySelector("body");
+  body.addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
+*/
